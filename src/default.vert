ce61668935604941cf3c63ad86f9602590b34590
Founder-TGL <ginsbergaiden@gmail.com>
2025-06-25 00:11:16 -0600

    SO MANY CHANGES, theoretically 3d camera works, I am simply implementing textures and better coordinates to represent this properly

--- src/default.vert
+++ src/default.vert
@@ -1,6 +1,27 @@
 #version 330 core
+// position/ cords
 layout (location = 0) in vec3 aPos;
+//color
+layout (location = 1) in vec3 aColor;
+
+//outputs the color for the fragment shader
+out vec3 color;
+
+//controls the scale of the vertices
+uniform float scale;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
+
+
 void main()
 {
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+   //outputs the positions and coordinates of the vertices
+   gl_Position = vec4(aPos.x + (aPos.x*scale), aPos.y + (aPos.y*scale), aPos.z + (aPos.z * scale), 1.0);
+   //model * view * proj* vec4(aPos, 1.0);
+   //
+   //assigns thbe colors from the vertex data to "color"
+   color = aColor;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 #include<glad/glad.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
 #include<GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
 #include"shaderClass.h"
 #include"VAO.h"
@@ -13,12 +17,12 @@
 // Vertices coordinates
 GLfloat vertices[] =
 {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 			0.8f, 0.3f, 0.02f,// Lower left corner
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 			0.8f, 0.3f, 0.02f,// Lower right corner
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 			1.0f, 0.6f, 0.32f,// Upper corner
+	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,			0.9f, 0.45f, 0.17f,// Inner left
+	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, 			0.9f, 0.45f, 0.17f,// Inner right
+	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f, 			0.8f, 0.3f, 0.02f// Inner down
 };
 
 // Indices for vertices order
@@ -29,6 +33,8 @@ GLuint indices[] =
 	5, 4, 1 // Upper triangle
 };
 
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 
 int main()
@@ -45,7 +51,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL window ;3", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -60,7 +66,7 @@ int main()
 	gladLoadGL();
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 
 
@@ -79,13 +85,15 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6*sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6*sizeof(float), (void*)(3 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-
+	
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -96,6 +104,21 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		view = glm::perspective(glm::radians(45.0f), (float)(width/height), 0.1f, 100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc,1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc,1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc,1, GL_FALSE, glm::value_ptr(proj));
+
+		glUniform1f(uniID, 0.5f);
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
